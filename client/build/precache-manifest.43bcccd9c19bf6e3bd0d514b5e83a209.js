0a9ae4bcd7cbc2c48503330df4aaf3cacc7fc083
Matthew Jaikaran <mattjaikaran@gmail.com>
2020-03-13 17:14:06 -0400

    updated gitignore for build file

---
+++ client/build/precache-manifest.43bcccd9c19bf6e3bd0d514b5e83a209.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8dfb86ce9b46e0885f7e1c12b4cf092",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df0b599cfd7233a11180",
+    "url": "/static/css/2.51c3026c.chunk.css"
+  },
+  {
+    "revision": "a38a0078b8330fc80200",
+    "url": "/static/css/main.3dffaf9a.chunk.css"
+  },
+  {
+    "revision": "df0b599cfd7233a11180",
+    "url": "/static/js/2.d7944029.chunk.js"
+  },
+  {
+    "revision": "432ed034d9831dc13cf75201c058fd1d",
+    "url": "/static/js/2.d7944029.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a38a0078b8330fc80200",
+    "url": "/static/js/main.d9b0fedf.chunk.js"
+  },
+  {
+    "revision": "08a01cac7a9424eef63f",
+    "url": "/static/js/runtime-main.ef76e641.js"
+  },
+  {
+    "revision": "585119cf0f530e6bd4b0fc2f03b74347",
+    "url": "/static/media/amex-white.585119cf.png"
+  },
+  {
+    "revision": "d035fb267bcbaf4bdf6a0b6e6061eb6b",
+    "url": "/static/media/coin.d035fb26.png"
+  },
+  {
+    "revision": "a4f6238bb9a2d4b32b71d3fffa7c6f15",
+    "url": "/static/media/coned.a4f6238b.png"
+  },
+  {
+    "revision": "d3d4719fef69400f1e174101caf00d24",
+    "url": "/static/media/piggy-bank.d3d4719f.png"
+  },
+  {
+    "revision": "a162f1ee10d257abc6325e58fbb2a51a",
+    "url": "/static/media/spotify.a162f1ee.png"
+  }
+]);
