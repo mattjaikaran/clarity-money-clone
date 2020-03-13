ef10c20286d4d6f8d3f0ddda3e2417928aa9f120
Matthew Jaikaran <mattjaikaran@gmail.com>
2020-03-12 23:21:57 -0400

    updated CreditCardDebt component to material ui and added slider

--- client/src/containers/CreditCardDebt/index.js
+++ client/src/containers/CreditCardDebt/index.js
@@ -1,63 +1,67 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import Typography from '@material-ui/core/Typography'
+import Carousel from 'react-material-ui-carousel'
+import Paper from '@material-ui/core/Paper'
+
+const useStyles = makeStyles({
+  root: {
+    marginTop: '1em',
+    width: '100%',
+    textAlign: 'center'
+  },
+  creditCardTotal: {
+    color: 'black'
+  },
+})
+
+var items = [
+  {
+    name: 'American Express Gold',
+    balance: 420.24,
+    company: 'Amex',
+    lastFour: 1234,
+    availableCredit: 958.32,
+    minDue: 0,
+  },
+  {
+    name: 'Chase Sapphire',
+    balance: 4320.24,
+    company: 'Chase',
+    lastFour: 4321,
+    availableCredit: 1973.38,
+    minDue: 0,
+  },
+]
+
+const Item = (props) => (
+  <Paper>
+    <Typography variant="body2">
+      {props.item.name}
+    </Typography>
+    <Typography variant="body">
+      {props.item.balance}
+    </Typography>
+  </Paper>
+)
 
 const CreditCardDebt = (props) => {
+  const classes = useStyles()
+
   return (
-    <div className="creditCardDebt col-md-4 col-sm-6 card shadow-sm">
-      <div className="containerInner">
-        <h6 className="font-weight-light text-center">
-          <span className="text-muted">Credit Cards:</span> $1632.13
-        </h6>
+    <Card className={classes.root}>
+      <CardContent>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          Credit Cards: <span className={classes.creditCardTotal}>$2326.25</span>
+        </Typography>
         <hr />
-        <div className="chaseCreditCard card containerInner shadow-lg">
-          <div className="row mt-2">
-            <div className="col-2 text-center">
-              <img src={require('../../img/chase.png')} className="transaction-img" alt="cc-logo" />
-            </div>
-            <div className="col-6 text-center">
-              <small className="font-weight-light">Chase Sapphire Card</small>
-            </div>
-            <div className="col-1">
-              <a href="#!">
-                <img src={require('../../img/edit.png')} className="edit-img" alt="" />
-              </a>
-            </div>
-            <div className="col-3 text-center">
-              <span className="font-weight-light">3609</span>
-            </div>
-          </div>
-          <div className="mt-3 containerInner">
-            <small className="font-weight-light">Balance</small>
-            <h6 className="font-weight-normal">$205.32</h6>
-            <div className="row mt-5 mb-2 text-black">
-              <div className="col-5">
-                <small className="font-weight-light">
-                  Available Credit
-                </small>
-                <p className="font-weight-normal">
-                  $8320.32
-                </p>
-              </div>
-              <div className="col-4">
-                <small className="font-weight-light">
-                  Minimum Due
-                </small>
-                <p className="font-weight-normal">
-                  $0.00
-                </p>
-              </div>
-              <div className="col-3">
-                <small className="font-weight-light">
-                  APR
-                </small>
-                <p className="font-weight-normal">
-                  10.25%
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+        <Carousel>
+          {items.map(item => <Item key={item} item={item} />)}
+        </Carousel>
+      </CardContent>
+    </Card>
   )
 }
 
