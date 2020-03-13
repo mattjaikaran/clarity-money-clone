2c8af64cb58eeffed73ffeca51754962c3192af3
Matthew Jaikaran <mattjaikaran@gmail.com>
2020-03-12 22:46:53 -0400

    updated CreditScore component to material ui

--- client/src/containers/CreditScore/index.js
+++ client/src/containers/CreditScore/index.js
@@ -1,34 +1,64 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import Typography from '@material-ui/core/Typography'
+
+const useStyles = makeStyles({
+  root: {
+    marginTop: '1em',
+    width: '100%',
+    textAlign: 'center'
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  graph: {
+    margin: '1em 0'
+  },
+  creditScoreRating: {
+    color: '#22d0a5'
+  },
+  pos: {
+    margin: '10px 0',
+  },
+  disclaimer: {
+    textDecoration: 'none'
+  },
+  learnMore: {
+    textDecoration: 'none'
+  },
+})
 
 const CreditScore = (props) => {
+  const classes = useStyles()
+  
   return (
-    <div className="creditScore col-md-4 col-sm-6 card shadow-sm">
-      <div className="containerInner text-center">
-        <div className="mt-0 mb-2">
-          <h6 className="d-inline font-weight-normal">
-            Your score: <span className="text-green">Good</span>*
-          </h6>
-        </div>
-
-        <hr className="transaction-hr" />
-
-        <div className="mt-3">
-          <h6 className="d-inline font-weight-light">
-            *credit score graph*
-          </h6>
-          <p className="mt-3 font-weight-light">
-            Your credit score hasn't changed since <br />the last report.
-          </p>
-          <small className="credit-score-updated-text text-muted mb-2 d-block">
-            Updated January 30
-          </small>
-          <small className="disclaimer-alt">
-            *VantageScore 3.0&reg; powered by Experian&reg;
-            <a href="#!">Learn More</a>
-          </small>
-        </div>
-      </div>
-    </div>
+    <Card className={classes.root}>
+      <CardContent>
+        <Typography >
+          Your score: <span className={classes.creditScoreRating}>Good</span>*
+        </Typography>
+        <hr />
+        {/* <Typography className={classes.graph} color="textSecondary" gutterBottom>
+          *Graph*
+        </Typography> */}
+        <img className={classes.graph} src={require('../../img/credit-score-graph.png')} />
+        <Typography variant="body" component="p">
+          Your credit score increased by 25 points
+        </Typography>
+        <Typography
+          className={classes.pos}
+          color="textSecondary" variant="caption" component="p">
+          Updated on February 29
+        </Typography>
+        <Typography variant="caption" component="p">
+          *VantageScore 3.0® powered by Experian® <a className={classes.learnMore} href="#!">Learn More</a>
+        </Typography>
+      </CardContent>
+    </Card>
   )
 }
 
