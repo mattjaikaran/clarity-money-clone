43a6367b881930b1aa1667fd22e6ed0c5297ff1f
Matthew Jaikaran <mattjaikaran@gmail.com>
2020-03-12 23:33:34 -0400

    converted data to material ui table

--- client/src/containers/Balance/index.js
+++ client/src/containers/Balance/index.js
@@ -4,15 +4,24 @@ import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import Typography from '@material-ui/core/Typography'
 import Grid from '@material-ui/core/Grid'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableContainer from '@material-ui/core/TableContainer'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+import Paper from '@material-ui/core/Paper'
 
 const useStyles = makeStyles({
   root: {
-    minWidth: 275,
-    padding: '0 1em',
+    width: '100%',
     flexGrow: 1
   },
   top: {
     marginTop: '0'
+  },
+  table: {
+    width: '100%',
   },
   title: {
     fontSize: 14,
@@ -28,13 +37,37 @@ const useStyles = makeStyles({
   },
 })
 
+function createData(name, amount) {
+  return { name, amount }
+}
+
+const rows = [
+  createData('Cash', 4470.39),
+  createData('Credit Debt', 1038.22),
+]
+
+
 const Balance = (props) => {
   const classes = useStyles()
 
   return (
     <Card className={classes.root}>
       <CardContent>
-        <Grid className={classes.top} container spacing={3}>
+        <Table className={classes.table} size="medium" aria-label="a medium sized table">
+          <TableBody>
+            {rows.map(row => (
+              <TableRow key={row.name}>
+                <TableCell component="th" scope="row">
+                  <Typography variant="body2" component="p">
+                    {row.name}
+                  </Typography>
+                </TableCell>
+                <TableCell align="right">${row.amount.toFixed(2)}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        {/* <Grid className={classes.top} container spacing={3}>
           <Grid item xs={6}>
             <Typography variant="body2" component="p">
               Cash
@@ -58,7 +91,7 @@ const Balance = (props) => {
               $1,038.22
             </Typography>
           </Grid>
-        </Grid>
+        </Grid> */}
       </CardContent>
     </Card>
   )
