6b7fce3e34c06c00038209fed4c25e92c302aa58
Matthew Jaikaran <mattjaikaran@gmail.com>
2020-03-13 14:12:56 -0400

    updated HowItWorks container to material ui

--- client/src/containers/HowItWorks/index.js
+++ client/src/containers/HowItWorks/index.js
@@ -1,18 +1,51 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+
+const useStyles = makeStyles({
+  root: {
+    backgroundColor: '#192a56',
+    color: 'white',
+    marginTop: '1em',
+    width: '100%',
+    textAlign: 'center',
+  },
+  title: {
+    marginTop: '1em'
+  },
+  phoneImg: {
+    maxWidth: '50px',
+    margin: '2em 0',
+  },
+  paragraph: {
+    padding: '0 19%'
+  }
+})
 
 const HowItWorks = (props) => {
+  const classes = useStyles()
+  
   return (
-    <div className="howItWorks col-md-4 col-sm-6 card bg-darkblue mb-3 shadow-sm">
-      <div className="containerInner p-3 text-center">
-        <h6 className="font-weight-normal">
-          How it works
-        </h6>
-        <img src={require('../../img/phone.png')} className="my-5 coin-img" alt="" />
-        <p className="copy font-weight-light">
+    <Card className={classes.root}>
+      <CardContent>
+        <Typography className={classes.title}>
+          How It Works
+        </Typography>
+        <div>
+          <img
+            className={classes.phoneImg}
+            src={require('../../img/phone.png')}
+            alt="phone" />
+        </div>
+        <Typography className={classes.paragraph} display="block" variant="caption">
           We are always working on new ways to help you, so over time you'll see new tiles appear in your feed.
-        </p>
-      </div>
-    </div>
+        </Typography>
+      </CardContent>
+    </Card>
   )
 }
 
