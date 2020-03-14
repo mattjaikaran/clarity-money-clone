d58be1aadb09739bc4b604c6a58ddcec86a237f7
Matthew Jaikaran <mattjaikaran@gmail.com>
2020-03-14 16:42:06 -0400

    added initial desktop/tablet responsiveness. built mobile footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid'
 import './App.css'
+import { makeStyles } from '@material-ui/core/styles'
+import GridList from '@material-ui/core/GridList'
+import GridListTile from '@material-ui/core/GridListTile'
 
 import Balance from './containers/Balance'
 import Header from './containers/Header'
@@ -19,26 +22,66 @@ import HowItWorks from './containers/HowItWorks'
 import ShareWithFriends from './containers/ShareWithFriends'
 import ThankYou from './containers/ThankYou'
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+}))
+
 function App() {
+  const classes = useStyles()
+
   return (
     <div className="app">
-      <Header />
-      <Balance />
-      <Grid container>
-        <Spending />
-        <Transactions />
-        <Loan />
-        <ConnectAccounts />
-        <Income />
-        <MonthBreakdown />
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6} md={4}>
+          <Header />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <Balance />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <Spending />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <Transactions />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <Loan />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <ConnectAccounts />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <Income />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <MonthBreakdown />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <CancelMonthly />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <CreditScore />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <CreditCardDebt />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <HowItWorks />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <ShareWithFriends />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <ThankYou />
+        </Grid>
         {/* <WhereISpend />
         <MonthGraph /> */}
-        <CancelMonthly />
-        <CreditScore />
-        <CreditCardDebt />
-        <HowItWorks />
-        <ShareWithFriends />
-        <ThankYou />
       </Grid>
     </div>
   )
--- client/src/containers/Footer/index.js
+++ client/src/containers/Footer/index.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import BottomNavigation from '@material-ui/core/BottomNavigation'
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction'
+import DashboardIcon from '@material-ui/icons/Dashboard'
+import PersonIcon from '@material-ui/icons/Person'
+import SearchIcon from '@material-ui/icons/Search'
+import AccountBalanceIcon from '@material-ui/icons/AccountBalance'
 
-const Footer = () => {
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+  },
+})
+
+export default function Footer() {
+  const classes = useStyles()
+  const [value, setValue] = React.useState(0)
+
   return (
-    <div>Footer</div>
+    <BottomNavigation
+      value={value}
+      onChange={(event, newValue) => {
+        setValue(newValue)
+      }}
+      showLabels
+      className={classes.root}
+    >
+      <BottomNavigationAction label="Dashboard" icon={<DashboardIcon />} />
+      <BottomNavigationAction label="Accounts" icon={<AccountBalanceIcon />} />
+      <BottomNavigationAction label="Transactions" icon={<SearchIcon />} />
+      <BottomNavigationAction label="Profile" icon={<PersonIcon />} />
+    </BottomNavigation>
   )
 }
-
-export default Footer
