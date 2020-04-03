477e10a470f2ba8301074ab51fb817b33b306eae
Matthew Jaikaran <mattjaikaran@gmail.com>
2020-04-02 21:22:57 -0400

    added API, started user schema

---
+++ server/src/models/users.model.js
@@ -0,0 +1,58 @@
+// users-model.js - A mongoose model
+//
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
+module.exports = function (app) {
+  const modelName = 'users';
+  const mongooseClient = app.get('mongooseClient');
+  const schema = new mongooseClient.Schema({
+  
+    email: { type: String, unique: true, lowercase: true },
+    password: { type: String },
+    account: {
+      balance: {
+        cash: { type: Number },
+        creditCardDebt: { type: Number },
+      },
+      transactions: [
+        {
+          name: { type: String },
+          amount: { type: Number }
+        }
+      ],
+      income: {
+        total: { type: Number },
+        spent: { type: Number },
+        balance: { type: Number }
+      },
+      monthlyBills: [
+        {
+          name: { type: String },
+          amount: { type: Number }
+        }
+      ],
+      creditScore: { type: Number },
+      creditCards: [
+        {
+          name: { type: String },
+          accountNo: { type: Number },
+          availableCredit: { type: Number },
+          balance: { type: Number },
+          minDue: { type: Number },
+          apr: { type: Number },
+        }
+      ]
+    },
+    googleId: { type: String },
+  }, {
+    timestamps: true
+  });
+
+  // This is necessary to avoid model compilation errors in watch mode
+  // see https://mongoosejs.com/docs/api/connection.html#connection_Connection-deleteModel
+  if (mongooseClient.modelNames().includes(modelName)) {
+    mongooseClient.deleteModel(modelName);
+  }
+  return mongooseClient.model(modelName, schema);
+
+};
